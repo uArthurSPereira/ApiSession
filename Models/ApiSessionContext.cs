2654b6274860b1dc04223ce789b6105f053e7fd0
uArthurSPereira <arthursp7007@gmail.com>
2020-07-01 16:40:21 -0300

    Db Mapeado

---
+++ Models/ApiSessionContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace ApiSession.Models
+{
+    public partial class ApiSessionContext : DbContext
+    {
+        public ApiSessionContext()
+        {
+        }
+
+        public ApiSessionContext(DbContextOptions<ApiSessionContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<TbFilme> TbFilme { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseMySql("server=localhost;user id=root;password=@tutubazz123;database=ApiSession", x => x.ServerVersion("8.0.19-mysql"));
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TbFilme>(entity =>
+            {
+                entity.HasKey(e => e.IdFilme)
+                    .HasName("PRIMARY");
+
+                entity.Property(e => e.DsGenero)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+
+                entity.Property(e => e.NmFilme)
+                    .HasCharSet("utf8mb4")
+                    .HasCollation("utf8mb4_0900_ai_ci");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
---
+++ Models/TbFilme.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiSession.Models
+{
+    [Table("tb_filme")]
+    public partial class TbFilme
+    {
+        [Key]
+        [Column("id_filme")]
+        public int IdFilme { get; set; }
+        [Required]
+        [Column("nm_filme", TypeName = "varchar(100)")]
+        public string NmFilme { get; set; }
+        [Required]
+        [Column("ds_genero", TypeName = "varchar(100)")]
+        public string DsGenero { get; set; }
+        [Column("nr_duracao")]
+        public int? NrDuracao { get; set; }
+        [Column("vl_avaliacao", TypeName = "decimal(10,2)")]
+        public decimal? VlAvaliacao { get; set; }
+        [Column("bt_disponivel")]
+        public bool BtDisponivel { get; set; }
+        [Column("dt_lancamento", TypeName = "datetime")]
+        public DateTime DtLancamento { get; set; }
+    }
+}
